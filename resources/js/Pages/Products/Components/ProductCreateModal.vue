034a53e61e5ebe1c8a0a7719c93cc5cd8ccfbdef
Nirison93 <94399812+Nirison93@users.noreply.github.com>
2026-01-26 21:14:28 +0530

    last chages

--- resources/js/Pages/Products/Components/ProductCreateModal.vue
+++ resources/js/Pages/Products/Components/ProductCreateModal.vue
@@ -217,7 +217,6 @@
                 v-model="form.purchase_price"
                 type="number"
                 step="0.01"
-                readonly
                 class="w-full px-3 py-2 text-sm text-gray-800 bg-gray-100 border border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-transparent"
                 placeholder="0.00"
               />
